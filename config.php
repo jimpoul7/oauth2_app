f1682311739de679a1da3777e9d37f73a4e6925c
jimpoul7 <jimpoul7@gmail.com>
2014-12-19 02:06:58 +0200

    Add php files

---
+++ config.php
@@ -0,0 +1,9 @@
+<?
+
+// config.php
+
+$oauth2_client_id = '1558';
+$oauth2_secret = '486e9140a2523ac3b6d3def9dfe501b08482b3d33ab1b94598a2ac49cfe30949399897849e6cd8fd6a6f171cb951d560efab2c3bf14fc2a52bb0eb09bc98aef4';
+$oauth2_redirect = 'http://130.206.127.174/main.php';
+
+?>
---
+++ main.php
@@ -0,0 +1,57 @@
+<?
+
+// main.php
+
+require('config.php');
+require('HttpPost.class.php');
+
+/**
+ * the OAuth server should have brought us to this page with a $_GET['code']
+ */
+ 
+ 
+if(isset($_GET['code'])) {
+    // try to get an access token
+    $code = $_GET['code'];
+    $token_url = 'https://account.lab.fi-ware.org/oauth2/token';
+    // this will be our POST data to send back to the OAuth server in exchange
+	// for an access token
+    $params = array(
+        "client_id" => $oauth2_client_id,
+        "client_secret" => $oauth2_secret,
+        "grant_type" => "authorization_code",
+        "code" => $code,
+        "redirect_uri" => $oauth2_redirect      
+                
+    );
+ 
+	// build a new HTTP POST request
+    $request = new HttpPost($token_url);
+    $request->setPostData($params);
+    $request->send();
+
+	// decode the incoming string as JSON
+    $responseObj = json_decode($request->getHttpResponse());
+
+    // construct url for user information
+    $user_url = 'https://account.lab.fi-ware.org/user?access_token=' . $responseObj->access_token ;
+
+    //echo "$user_url";
+
+    $ch = curl_init($user_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    $user_info = curl_exec($ch);
+
+    $userObj = json_decode($user_info);
+
+    echo "$userObj->id";
+    echo "$userObj->displayName";
+    echo "$userObj->email";
+
+    curl_close($ch);
+
+}
+
+
+?>
