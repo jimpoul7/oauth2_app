b975950243f23f91ff02dae48be750658daa447c
jimpoul7 <jimpoul7@gmail.com>
2014-12-20 23:30:26 +0200

    main , index changes

--- main.php
+++ main.php
@@ -9,11 +9,15 @@ require('HttpPost.class.php');
  
  
 if(isset($_GET['code'])) {
+
     // try to get an access token
+
     $code = $_GET['code'];
     $token_url = 'https://account.lab.fi-ware.org/oauth2/token';
+
     // this will be our POST data to send back to the OAuth server in exchange
 	// for an access token
+
     $params = array(
         "client_id" => $oauth2_client_id,
         "client_secret" => $oauth2_secret,
@@ -27,21 +31,28 @@ if(isset($_GET['code'])) {
     $request->setPostData($params);
     $request->send();
 
-	// decode the incoming string as JSON
+	// decode the response as JSON
     $responseObj = json_decode($request->getHttpResponse());
 
     // construct url for user information
     $user_url = 'https://account.lab.fi-ware.org/user?access_token=' . $responseObj->access_token ;
 
+    // send GET request for user information
     $ch = curl_init($user_url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HEADER, 0);
     $user_info = curl_exec($ch);
 
+    // decode the response as JSON
     $userObj = json_decode($user_info);
 
     curl_close($ch);
+}
 
+else {
+
+    //redirect user to index.php if there is no code 
+    header('Location: ' . 'http://130.206.127.174');
 }
 
 ?>
@@ -58,7 +69,9 @@ if(isset($_GET['code'])) {
     <?php
     echo " Id : $userObj->id";
     echo '<br/>';
-    echo "Name : $userObj->displayName";
+    echo "nickName : $userObj->nickName";
+    echo '<br/>';
+    echo "displayName : $userObj->displayName";
     echo '<br/>';
     echo "Email : $userObj->email";
     ?>
